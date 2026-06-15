51536f725200e5ae5fe30d6d414c4714a324aa85
NotSky012 <notsky0005@gmail.com>
2026-06-15 10:48:15 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "MTUxNTM1NjkxNjIwNzg0NTQyNg.GiADWo.oigmKqmVFCzdt8NGsfWZiquZ2uGYSxxrx9w7z4", 
+    token: "", 
     id: "1515356916207845426", 
     prefix: "!",
     expressPort: process.env.PORT || 10000
